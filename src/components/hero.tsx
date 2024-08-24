e624a72218daa44a3aefdcd5daef67e09686ca6c
Tubbster-UX <Alexavierbrink@gmail.com>
2024-08-24 10:11:03 -0600

    refactor: Update Hero component overlay opacity for better text visibility

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Hero() {
     return (
         <div className='relative flex flex-col justify-center items-center h-[45rem] bg-[url("/hero.avif")] bg-cover bg-center bg-fixed p-6 text-white'>
-            <div className='absolute inset-0 bg-black opacity-30'></div> {/* Overlay for better text visibility */}
+            <div className='absolute inset-0 bg-black opacity-60'></div> {/* Overlay for better text visibility */}
             <div className='relative z-10 text-center'>
                 <h1 className='text-4xl font-extrabold mb-4'>Turning Ideas into Innovative Solutions</h1>
                 <p className='text-lg mb-6'>Transform your digital presence with cutting-edge design and development tailored to your vision.</p>

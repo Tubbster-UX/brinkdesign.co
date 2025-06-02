3b67d377cc0979d849db4acf95990b696d280e31
Tubbster-UX <Alexavierbrink@gmail.com>
2025-06-02 10:27:37 -0600

    Add Google Analytics integration using @next/third-parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import ConditionalLayout from "@/components/ConditionalLayout";
 import CalendlyEmbed from "@/components/CalendlyEmbed";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Brink Design Co. - Innovative Web Solutions",
@@ -65,7 +66,7 @@ export default function RootLayout({
           {children}
           <CalendlyEmbed url="https://calendly.com/brinkdesignco/30min" />
         </ConditionalLayout>
-
+        <GoogleAnalytics gaId="G-K6VB75FMGH" />
       </body>
     </html>
   );

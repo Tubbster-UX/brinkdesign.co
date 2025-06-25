4861e822c3f0b66fc4b5713b0730fc39341f852a
Tubbster-UX <Alexavierbrink@gmail.com>
2025-06-25 12:15:44 -0600

    Refactor sitemap function to include specific blog, projects, and images URLs

--- src/app/index/sitemap.ts
+++ src/app/index/sitemap.ts
@@ -1,11 +1,22 @@
 import { MetadataRoute } from 'next';
 
-export default function sitemapIndex(): MetadataRoute.Sitemap {
-  const now = new Date();
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: 'https://www.brinkdesign.co/sitemap.xml', lastModified: now },
-    { url: 'https://www.brinkdesign.co/sitemap-blog.xml', lastModified: now },
-    { url: 'https://www.brinkdesign.co/sitemap-projects.xml', lastModified: now },
-    { url: 'https://www.brinkdesign.co/sitemap-images.xml', lastModified: now },
+    {
+      url: 'https://www.brinkdesign.co/sitemap.xml',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.brinkdesign.co/blog/sitemap.xml',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.brinkdesign.co/projects/sitemap.xml',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.brinkdesign.co/images/sitemap.xml',
+      lastModified: new Date(),
+    },
   ];
 }

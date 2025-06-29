d6f2aa4784e6ebc1fdc71e6c5c2a5c4591edb595
Tubbster-UX <Alexavierbrink@gmail.com>
2025-06-29 10:22:51 -0600

    Fix Line Card link URL in header navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -64,7 +64,7 @@ export default function Header() {
                         </li>
                         <li>
                             <Link
-                                href="/line-card"
+                                href="/linecard"
                                 className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                             >
                                 Line Card

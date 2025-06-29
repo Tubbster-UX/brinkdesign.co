be943026175ec49226181123dc02a59d72c12d19
Tubbster-UX <Alexavierbrink@gmail.com>
2025-06-29 10:23:26 -0600

    Add Line Card URL to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,6 +7,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     { url: 'https://www.brinkdesign.co/services', lastModified: now },
     { url: 'https://www.brinkdesign.co/projects', lastModified: now },
     { url: 'https://www.brinkdesign.co/blog', lastModified: now },
+    { url: 'https://www.brinkdesign.co/linecard', lastModified: now },
     { url: 'https://www.brinkdesign.co/contact', lastModified: now },
     { url: 'https://www.brinkdesign.co/service-area', lastModified: now },
     { url: 'https://www.brinkdesign.co/faq', lastModified: now },
